3ba9e4a1fa75fb0957d3ec116cedd2c140b0bbd3
David Martínez <goddrinksjava@gmail.com>
2024-10-21 23:09:43 -0600

    Add support for simulating ADD, SUB, and CMP

--- src/cpu_state.rs
+++ src/cpu_state.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use crate::decode::{decode_instruction, CountingPeekable};
 use crate::instruction::Instruction;
 use crate::memory::Address;
@@ -5,10 +7,54 @@ use crate::operand::Operand;
 use crate::register::Register;
 use anyhow::Result;
 
+#[derive(Clone, PartialEq, Eq)]
+pub struct CpuStateFlags {
+    flags: u8,
+}
+
+impl CpuStateFlags {
+    const ZERO_FLAG_MASK: u8 = 0b0000_0001;
+    const SIGN_FLAG_MASK: u8 = 0b0000_0010;
+
+    pub fn new() -> CpuStateFlags {
+        CpuStateFlags { flags: 0 }
+    }
+
+    pub fn set_zero_flag(&mut self, v: bool) {
+        if v {
+            self.flags |= Self::ZERO_FLAG_MASK;
+        } else {
+            self.flags &= !Self::ZERO_FLAG_MASK;
+        }
+    }
+
+    pub fn set_sign_flag(&mut self, v: bool) {
+        if v {
+            self.flags |= Self::SIGN_FLAG_MASK;
+        } else {
+            self.flags &= !Self::SIGN_FLAG_MASK;
+        }
+    }
+}
+
+impl fmt::Display for CpuStateFlags {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut flags_str = String::new();
+        if self.flags & Self::ZERO_FLAG_MASK != 0 {
+            flags_str.push('Z');
+        }
+        if self.flags & Self::SIGN_FLAG_MASK != 0 {
+            flags_str.push('S');
+        }
+        write!(f, "{}", flags_str)
+    }
+}
+
 pub struct CpuState<'a> {
     instructions: &'a [u8],
     instruction_pointer: u16,
     registers: [u16; 8],
+    flags: CpuStateFlags,
 }
 
 impl<'a> CpuState<'a> {
@@ -17,6 +63,7 @@ impl<'a> CpuState<'a> {
             instructions,
             instruction_pointer: 0,
             registers: [0; 8],
+            flags: CpuStateFlags::new(),
         }
     }
 
@@ -51,7 +98,7 @@ impl<'a> CpuState<'a> {
 
     fn run_next_instruction(&mut self) -> Result<()> {
         let instruction = self.decode_instruction()?;
-        println!(
+        print!(
             "Executing {} at 0x{:x}",
             instruction, self.instruction_pointer
         );
@@ -61,16 +108,90 @@ impl<'a> CpuState<'a> {
         match instruction {
             Instruction::Mov { dst, src, .. } => match dst {
                 Operand::Register(reg) => {
+                    let previous_flags = self.flags.clone();
+
                     let (index, mask) = self.get_register_index_and_mask(&reg);
+                    let previous_register_value = self.registers[index] & mask;
                     self.registers[index] = self.get_operand_value(&src) & mask;
+                    print!(
+                        "; {}: 0x{:x} -> 0x{:x}",
+                        reg,
+                        previous_register_value,
+                        self.registers[index] & mask
+                    );
+                    if previous_flags != self.flags {
+                        print!("; flags:{} -> flags:{}", previous_flags, self.flags);
+                    }
                 }
                 Operand::Memory(_) => todo!(),
                 Operand::Immediate8(_) => unreachable!(),
                 Operand::Immediate16(_) => unreachable!(),
             },
-            Instruction::Add { .. } => todo!(),
-            Instruction::Sub { .. } => todo!(),
-            Instruction::Cmp { .. } => todo!(),
+            Instruction::Add { dst, src, .. } => match dst {
+                Operand::Register(reg) => {
+                    let previous_flags = self.flags.clone();
+
+                    let (index, mask) = self.get_register_index_and_mask(&reg);
+                    let previous_register_value = self.registers[index] & mask;
+                    let src_value = self.get_operand_value(&src);
+                    let new_register_value = self.add_to_register(&reg, src_value);
+                    self.registers[index] = new_register_value;
+
+                    print!(
+                        "; {}: 0x{:x} -> 0x{:x}",
+                        reg,
+                        previous_register_value,
+                        self.registers[index] & mask
+                    );
+
+                    if previous_flags != self.flags {
+                        print!("; flags:{} -> flags:{}", previous_flags, self.flags);
+                    }
+                }
+                Operand::Memory(_) => todo!(),
+                Operand::Immediate8(_) => unreachable!(),
+                Operand::Immediate16(_) => unreachable!(),
+            },
+            Instruction::Sub { dst, src, .. } => match dst {
+                Operand::Register(reg) => {
+                    let previous_flags = self.flags.clone();
+
+                    let (index, mask) = self.get_register_index_and_mask(&reg);
+                    let previous_register_value = self.registers[index] & mask;
+                    let src_value = self.get_operand_value(&src);
+                    let new_register_value = self.add_to_register(&reg, src_value.wrapping_neg());
+                    self.registers[index] = new_register_value;
+
+                    print!(
+                        "; {}: 0x{:x} -> 0x{:x}",
+                        reg,
+                        previous_register_value,
+                        self.registers[index] & mask
+                    );
+
+                    if previous_flags != self.flags {
+                        print!("; flags:{} -> flags:{}", previous_flags, self.flags);
+                    }
+                }
+                Operand::Memory(_) => todo!(),
+                Operand::Immediate8(_) => unreachable!(),
+                Operand::Immediate16(_) => unreachable!(),
+            },
+            Instruction::Cmp { dst, src, .. } => match dst {
+                Operand::Register(reg) => {
+                    let previous_flags = self.flags.clone();
+
+                    let src_value = self.get_operand_value(&src);
+                    self.add_to_register(&reg, src_value.wrapping_neg());
+
+                    if previous_flags != self.flags {
+                        print!("; flags:{} -> flags:{}", previous_flags, self.flags);
+                    }
+                }
+                Operand::Memory(_) => todo!(),
+                Operand::Immediate8(_) => unreachable!(),
+                Operand::Immediate16(_) => unreachable!(),
+            },
             Instruction::Je { .. } => todo!(),
             Instruction::Jl { .. } => todo!(),
             Instruction::Jle { .. } => todo!(),
@@ -93,7 +214,41 @@ impl<'a> CpuState<'a> {
             Instruction::Jcxz { .. } => todo!(),
         }
 
+        println!();
         Ok(())
+    }
+
+    fn add_to_register(&mut self, dst: &Register, src_value: u16) -> u16 {
+        let (index, mask) = self.get_register_index_and_mask(&dst);
+
+        if mask == 0xFFFF {
+            let value = (self.registers[index]).wrapping_add(src_value);
+
+            self.flags.set_zero_flag(value == 0);
+            self.flags
+                .set_sign_flag((value & 0b1000_0000_0000_0000) != 0);
+
+            value
+        } else {
+            let src_value = if mask == 0xFF00 {
+                ((src_value & mask) >> 8) as u8
+            } else {
+                (src_value & mask) as u8
+            };
+
+            let dst_value = if mask == 0xFF00 {
+                ((self.registers[index] & mask) >> 8) as u8
+            } else {
+                (self.registers[index] & mask) as u8
+            };
+
+            let value = dst_value.wrapping_add(src_value);
+
+            self.flags.set_zero_flag(value == 0);
+            self.flags.set_sign_flag((value & 0b1000_0000) != 0);
+
+            (value as u16 & mask) & (self.registers[index] & mask.swap_bytes())
+        }
     }
 
     fn get_operand_value(&self, op: &Operand) -> u16 {
